4e6bc73a2c7982e59b2de6094d003b0f4575c809
Vinicius Laurindo <tecnicolaurindo@gmail.com>
2026-06-27 21:13:48 -0400

    Add new file for Tipos fixos Status Financeiro

---
+++ src/main/java/br/com/caimbebasketball/model/enums/ItemEstoque.java
@@ -0,0 +1,9 @@
+package br.com.caimbebasketball.model.enums;
+
+public class ItemEstoque {
+    private Long id;
+    private String descricao; // Ex: Camisa Treino Oficial
+    private String tamanho;   // Ex: P, M, G, GG
+    private Integer quantidadeDisponivel;
+    private Double valorUnitario;
+}
